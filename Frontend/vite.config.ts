2c85d00175cc488725d17c06683c7033c3d6a46f
Charles <emaildocharles@gmail.com>
2026-06-05 23:49:18 -0300

    chore: force vercel rebuild

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Build v2
 export default defineConfig({
   plugins: [react()],
   server: {
